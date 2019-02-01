522afc4cfac069dec21c1d9e6e3103596169a42f
Connor Bailey <bailey.connor.p@gmail.com>
2019-02-01 15:39:30 -0600

    journal entry form is now created when page is loaded by .mjs

--- src/scripts/journal.mjs
+++ src/scripts/journal.mjs
@@ -1,7 +1,9 @@
+import populateJournalForm from './populateJournalForm.mjs'
 import API from './APIFunctions.mjs'
 import clearDisplayedEntriesFromDOM from './clearDisplayedEntriesFromDOM.mjs'
 import resetJournalEntryForm from './resetJournalEntryForm.mjs'
 
+populateJournalForm();
 API.GET()
 
 document.querySelector("#recordButton").addEventListener("click", () => {
