dbb88110ab3394e58d21c5a0f19feee05f30da92
Connor Bailey <bailey.connor.p@gmail.com>
2019-02-06 14:26:08 -0600

    filter buttons call fetch entries from API, and post only entries that correspond to correct mood

--- src/scripts/turnPastEntriesToHTML.mjs
+++ src/scripts/turnPastEntriesToHTML.mjs
@@ -3,22 +3,47 @@ import getPastEntriesFromDatabase from "./getPastEntriesFromDatabase.mjs"
 function turnPastEntriesToHTML() {
     return getPastEntriesFromDatabase().then((pastEntriesArray) => {
         let pastEntryHTMLBlocks = [];
-        pastEntriesArray.forEach(entry => {
-            pastEntryHTMLBlocks.push(
-                `
-                <div id="${entry.date}" class="pastEntry">
-                    <h1 class="pastEntryTitle">${entry.conceptsCovered}</h1>
-                    <div class="pastEntryText">${entry.textEntry}</div>
-                    <div class="pastEntryLink">
-                       <a href="${entry.link}">Related Link: ${entry.linkDescription} </a>
-                    </div>
-                    <div class="dateAndMood">
-                       <div class="pastEntryDate">${entry.date}</div>
-                       <div class="pastEntryMood">${entry.mood}</div>
+        let moodOptionsArray = [].slice.call(document.querySelectorAll(".radio"))
+        if (moodOptionsArray[0].checked) {
+            pastEntriesArray.forEach(entry => {
+                pastEntryHTMLBlocks.push(
+                    `
+                    <div id="${entry.date}" class="pastEntry">
+                        <h1 class="pastEntryTitle">${entry.conceptsCovered}</h1>
+                        <div class="pastEntryText">${entry.textEntry}</div>
+                        <div class="pastEntryLink">
+                           <a href="${entry.link}">Related Link: ${entry.linkDescription} </a>
+                        </div>
+                        <div class="dateAndMood">
+                           <div class="pastEntryDate">${entry.date}</div>
+                           <div class="pastEntryMood">${entry.mood}</div>
+                       </div>
                    </div>
-               </div>
-               `)
-        })
+                   `)
+            })
+        } else {
+            let moodToShow = moodOptionsArray.filter(moodOption => {
+                return moodOption.checked === true
+            })[0].id
+            pastEntriesArray.filter(pastEntry => {
+                return pastEntry.mood === moodToShow
+            }).forEach(entry => {
+                pastEntryHTMLBlocks.push(
+                    `
+                    <div id="${entry.date}" class="pastEntry">
+                        <h1 class="pastEntryTitle">${entry.conceptsCovered}</h1>
+                        <div class="pastEntryText">${entry.textEntry}</div>
+                        <div class="pastEntryLink">
+                           <a href="${entry.link}">Related Link: ${entry.linkDescription} </a>
+                        </div>
+                        <div class="dateAndMood">
+                           <div class="pastEntryDate">${entry.date}</div>
+                           <div class="pastEntryMood">${entry.mood}</div>
+                       </div>
+                   </div>
+                   `)
+            })
+        }
         return pastEntryHTMLBlocks
     })
 }
