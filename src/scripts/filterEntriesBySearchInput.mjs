6c5fcc45e4422fc6d8cc9505702deadcfe1af4e6
Connor Bailey <bailey.connor.p@gmail.com>
2019-02-07 15:53:34 -0600

    fixed text search input to match search input regardless of upper/lowercase

--- src/scripts/filterEntriesBySearchInput.mjs
+++ src/scripts/filterEntriesBySearchInput.mjs
@@ -3,9 +3,9 @@ import filterEntriesBySelectedMood from "./filterEntriesBySelectedMood.mjs"
 function filterEntriesBySearchInput() {
     return filterEntriesBySelectedMood()
         .then((entriesFilteredByMood) => {
-            let input = document.querySelector("#searchEntriesInput").value
+            let input = document.querySelector("#searchEntriesInput").value.toLowerCase()
             let entriesFilteredBySearchInput = entriesFilteredByMood.filter((object) => {
-                return (object.conceptsCovered.includes(input) || object.textEntry.includes(input))
+                return (object.conceptsCovered.toLowerCase().includes(input) || object.textEntry.toLowerCase().includes(input))
             })
             return entriesFilteredBySearchInput
         })
